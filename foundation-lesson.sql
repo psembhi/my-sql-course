596a6c1186daa7346add6ff0a5c3614f79e4a59f
psembhi <pam.sembhi@nhs.net>
2025-04-30 15:08:55 +0000

    END OF COURSE

--- foundation-lesson.sql
+++ foundation-lesson.sql
@@ -4,7 +4,6 @@ SELECT
 ps.PatientId
 ,ps.Hospital
 ,ps.Ward
---,'Reminderdate' as ReminderDate
 ,DATEADD(WEEK, -12, ps.AdmittedDate) as Reminderdate
 ,ps.AdmittedDate 
 ,ps.DischargeDate
@@ -16,7 +15,37 @@ WHERE ps.Hospital  in ('PRUH', 'Oxleas')
 and ps.Ward LIKE '%Surgery'
 --AND ps.AdmittedDate >= '2024-02-27'
 --AND ps.AdmittedDate <= '2024-03-01'
-AND ps.AdmittedDate between '2024-02-27' and '2024-03-01'
+--AND ps.AdmittedDate between '2024-02-27' and '2024-03-01'
+ORDER BY ps.AdmittedDate DESC, ps.PatientId DESC
 
+---AGG Function 
+SELECT 
+ps.Hospital
+,ps.Ward
+,count(*) as  NumberOfPatient
+,sum(ps.Tariff) as Tariff
+,avg(ps.Tariff ) as Avg
+,max(ps.Tariff) as MaxTariff
+,Min(ps.Tariff) as MinTariff
+FROM PatientStay ps
+GROUP BY  ps.Hospital
+,ps.Ward
+ORDER BY NumberOfPatient DESC
+
+-- Joins Table
+
+SELECT
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.Hospital
+    ,dh.HospitalSize
+    ,dh.HospitalType
+FROM
+    PatientStay ps INNER JOIN DimHospital dh ON ps.hospital = dh.Hospital
+    WHERE dh.HospitalType IS NULL
+   
+
+SELECT * FROM DimHospitalBad
+    
 
 
